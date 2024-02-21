064fb182b3cf422ea6cc813e626163417287051e
Colin <colin@rkg.com>
2024-02-21 00:35:26 -0800

    Modified function input and output to use negatives, updated readme.md to reflect usage.

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 // coordinateConverter.js
 
-function dmsToDecimal(degrees, minutes, seconds, direction) {
-  let decimal = degrees + minutes / 60 + seconds / 3600;
-  if (direction === "S" || direction === "W") {
-    decimal = -decimal;
-  }
-  return decimal;
+function dmsToDecimal(degrees, minutes, seconds) {
+  let decimal =
+    Math.sign(degrees) * (Math.abs(degrees) + minutes / 60 + seconds / 3600);
+  return Number(decimal.toFixed(8));
 }
 
 function decimalToDMS(decimal) {
-  const degrees = Math.floor(Math.abs(decimal));
-  const minutes = Math.floor((Math.abs(decimal) - degrees) * 60);
-  const seconds = ((Math.abs(decimal) - degrees - minutes / 60) * 3600).toFixed(
-    2
+  const degrees = Math.sign(decimal) * Math.floor(Math.abs(decimal));
+  const minutes = Math.floor((Math.abs(decimal) - Math.abs(degrees)) * 60);
+  const seconds = Number(
+    ((Math.abs(decimal) - Math.abs(degrees) - minutes / 60) * 3600).toFixed(2)
   );
 
-  const direction = decimal >= 0 ? (decimal === 0 ? "" : "N") : "S";
-
-  return {
-    degrees,
-    minutes,
-    seconds,
-    direction,
-  };
+  return [degrees, minutes, seconds];
 }
 
 module.exports = {
